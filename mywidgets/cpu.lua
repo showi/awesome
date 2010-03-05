677b91515d0682be5ceef79f284327b781e70bdf
sho <joachim.basmaison@gmail.com>
2010-03-05 20:46:44 +0100

    No more mywidgets folder

--- mywidgets/cpu.lua
+++
@@ -1,9 +0,0 @@
-w_cpu = awful.widget.graph({ layout = awful.widget.layout.horizontal.rightleft})
-w_cpu:set_width(50)
-w_cpu:set_height(18)
-w_cpu:set_border_color("#000000")
-w_cpu:set_background_color("#494B4F")
-w_cpu:set_color("#FF5656")
-w_cpu:set_gradient_colors({ "#FF5656", "#88A175", "#AECF96" })
-vicious.register(w_cpu, vicious.widgets.cpu, "$1", 5)
-
--- mywidgets/cpufreq.lua
+++
@@ -1,7 +0,0 @@
-w_cpufreq = widget({ type = "textbox", layout = awful.widget.layout.horizontal.rightleft})
-w_cpufreq.background_color = "#494B4F"
-vicious.register(w_cpufreq, vicious.widgets.cpufreq,
-    function(widget, args)
-        return string.format("%5i MhZ", args[1])
-end, 7, "cpu0")
-
